c582fb24ae38d4f73906b3fc718a7be2987f84e6
Manya Gupta <142630781+manyagupta0209@users.noreply.github.com>
2023-11-05 23:05:29 +0530

    Update Q1.cpp

--- c++/Q1.cpp
+++ c++/Q1.cpp
@@ -1,17 +1,1 @@
-#include<iostream>
-using namespace std; 
-struct student {
-    int studentID;
-    double GPA;
-};
 
-void printStudentInfo(student s) {
-    std::cout<<"Student ID- "<<s.studentID<<std::endl;
-    std::cout<<"Student GPA- "<<s.GPA;
-}
-
-int main() {
-    student alice = {101, 3.8};
-    printStudentInfo(alice);
-    return 0;
-}
